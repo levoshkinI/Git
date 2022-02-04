4cd7008604cc5324c34de3b3318d0b3ebfd37b53
levoshkinI <98892049+levoshkinI@users.noreply.github.com>
2022-02-04 12:46:10 +0200

    Delete src directory

--- src/admin/views/showmenu.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if(!defined('MyConst')) {
-	   header('Location: /');
-	   die('Прямой доступ запрещен');
-	}
-?>
-	<ul>
-		<?php echo $cat_menu_ul; ?>
-	</ul>
--- src/admin/views/userinfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-?>
-	<div style="margin:10px 30px 0 75%">
-		Привет, <b><?= $_SESSION['email']?></b><br>
-		<form method="post" action="">
-			<button type="submit" class="btn btn-primary" name="exit">Выйти</button>
-		</form>
-	</div>
